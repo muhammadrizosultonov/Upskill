c993f8cb264fbf3e22e60c851660204f9b13349e
QudratullaevAbduazim <abduazimqudratullaev@gmail.com>
2026-01-18 19:58:02 +0500

    Login boldi logout bn

--- users/views.py
+++ users/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 class LoginView(View):
     def get(self, request):
         return render(request, 'users/login.html')
-
-
+        
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user:
+            login(request, user)
+            return redirect('users:profile')
+        else:
+            return render(request, 'users/login.html', {'error': 'Xatolik'})
+        
 
 class ProfileView(View):
     def get(self, request):
@@ -19,3 +31,11 @@ class ProfileView(View):
 class SettingsView(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'users/settings.html')
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'Siz tizimdan chiqdingiz.')
+        return redirect('users:login')
+    
